f5e2e8ca3aa01b0a9ec0cfc631664f136fd10929
Andrew Davis <andrew@diff.mx>
2016-08-07 21:35:44 -0500

    switch to webdriverio

--- test/e2e/input-mirrored.js
+++ test/e2e/input-mirrored.js
@@ -1,34 +1,27 @@
 
-browser.get('/')
+browser.url('/')
 
 describe('application loads', function () {
   it('should have test-application in the dom', function () {
-    browser.driver.isElementPresent(by.css('#test-application'))
-    .then(function (result) {
-      expect(result).toEqual(true)
-    })
+    browser.pause(1000)
+    var title = browser.getTitle()
+    expect(title).toBe('B E E F Y')
+    var visible = browser.isVisible('#test-application')
+    expect(visible).toBe(true)
   })
 })
 
 describe('events fire', function () {
+  it('should allow text entry', function (done) {
+    browser.setValue('#test-input', 'testing input')
+  })
   it('should mirror text as it is entered', function () {
-    browser.driver.findElement(by.css('#test-input'))
-    .sendKeys('testing input')
-    .then(function () {
-      return browser.driver.findElement(by.css('#echo-output')).getText()
-    })
-    .then(function (text) {
-      expect(text).toEqual('testing input')
-    })
+    var text = browser.getText('#echo-output')
+    expect(text).toEqual('testing input')
   })
   it('should update the dom when enter is pressed', function () {
-    browser.driver.findElement(by.css('#test-input'))
-    .sendKeys(protractor.Key.RETURN)
-    .then(function () {
-      return browser.driver.findElement(by.css('#enter-output')).getText()
-    })
-    .then(function (text) {
-      expect(text).toEqual('testing input')
-    })
+    browser.keys(['Enter'])
+    var text = browser.getText('#enter-output')
+    expect(text).toEqual('testing input')
   })
 })
